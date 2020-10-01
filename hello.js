407589597f1b55a4c271e750e2efa0eadab2162e
Ayrton Saué Cossuol <ayrton.cossuol@live.com>
2020-10-01 18:09:13 -0300

    Update hello.js
    Welcome message

--- hello.js
+++ hello.js
@@ -6,6 +6,7 @@ hello('Jonathan', 'Happy Hacktoberfest! Finally something good from 2020');
 hello("Aniket","Thankyou for making this Repository")
 hello("Jonas Alves","The best challenge - hacktoberfest 2020 | Brazil")
 hello("Erik","Hello there from mexico 🇲🇽")
+hello("Ayrton Cossuol","Best event of the year - Brazil, hacktoberfest 2020")
 
 
 const message =  (message) => {
